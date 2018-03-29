0268120ec3ee5d7540ffe9e3fd07855aa9d2414b
Arthanari <c.arthanari@gmail.com>
2018-03-28 21:32:25 -0400

    Update

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert(1);
+var cntrl_key_pressed=false;
+function onKeyDownHandler(e)
+{        
+
+}
+function onKeyUpHandler(e)
+{
+  if (e.ctrlKey) {
+      if (e.keyCode == 73 || e.keyCode == 73) {
+          console.log("Control Pressed");
+          e.preventDefault();
+          return false;
+      }
+  }
+}
